06e0a9e72498240a917a32e1f3dccb8043b1f253
MarkHW <lolka-pop@rambler.ru>
2023-05-23 20:02:03 +0300

    fix: change contacts.js, map-icon error

--- js/contacts.js
+++ js/contacts.js
@@ -1,7 +1,4 @@
 const init = () => {
-  const currentwidth = window.innerWidth;
-  const size = [currentwidth > 768 ? 118 : 98, currentwidth > 768 ? 118 : 98];
-
   const map = new ymaps.Map('root', {
     center: [55.807749, 37.570141],
     zoom: 18,
@@ -10,15 +7,13 @@ const init = () => {
     controls: []
   });
 
-
-
   const customIcon = new ymaps.Placemark(
     [55.807749, 37.570141], // Координаты метки
     {},
     {
       iconLayout: 'default#image',
-      iconImageHref: './img/contacts/geo-icon.svg', 
-      iconImageSize: size,
+      iconImageHref: './img/contacts/geo-icon.svg',
+      iconImageSize: [118, 118], // Размеры иконки для больших экранов (по умолчанию)
       iconImageOffset: [-16, -32]
     }
   );
@@ -31,13 +26,46 @@ const init = () => {
   map.controls.remove('zoomControl');
   map.controls.remove('rulerControl');
 
-  
   map.options.set({
-    suppressMapOpenBlock: true, 
+    suppressMapOpenBlock: true,
     suppressObsoleteBrowserNotifier: true
   });
 
   map.geoObjects.add(customIcon);
+
+  const swiper = new Swiper('.swiper-container', {
+    loop: false,
+    spaceBetween: 30,
+    slidesOffsetAfter: 26.5,
+    effect: 'slide',
+    speed: 550,
+    nested: true,
+    freeMode: true,
+    freeModeMomentum: true,
+    freeModeMomentumRatio: 0.5,
+    freeModeMomentumBounce: true,
+    freeModeMomentumBounceRatio: 0.3,
+    breakpoints: {
+      768: {
+        slidesPerView: 9.5,
+        spaceBetween: 10,
+        on: {
+          init: function () {
+            customIcon.options.set('iconImageSize', [118, 118]);
+          }
+        }
+      },
+      320: {
+        slidesPerView: 3.7,
+        spaceBetween: 10,
+        on: {
+          init: function () {
+            customIcon.options.set('iconImageSize', [98, 98]);
+          }
+        }
+      }
+    }
+  });
 };
 
 ymaps.ready(init);
